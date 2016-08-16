32ea3163c5a4536707ff01c893bf84e78423d7d5
jbrinkerhoff <jbrinkerhoff@A-1012517.ldschurch.org>
2016-08-16 12:53:34 -0600

    modified index.php structure

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/head.php'; ?>
-        <title>Home | JordanBrinkerhoff.net </title>
+        <title>Jordan</title>
     </head>
     <body>
         <div>
@@ -12,25 +12,47 @@
                     <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/header.php'; ?>
                 </div>
             </header>
-            <nav role="navigation">
-                <div>
+            <nav class="navbar-container" role="navigation">
+                <div class="brand">
                     <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/navigation.php'; ?>
+                </div>
+                <div class="about">
+
+                </div>
+                <div class="projects">
+
+                </div>
+                <div class="contact">
+
                 </div>
             </nav>
             <main role="main">
-                <div class="center">
+                <div class="intro-container">
                     <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/main.php'; ?>
-                    <h1>JordanBrinkerhoff.net</h1>
+                    <div class="intro-head"> <h1>JordanBrinkerhoff.net</h1>
 
-                    <p>This is a web site for a class, CIT 336 at BYU-Idaho!</p>
+
+                    </div>
+                    <div class="intro-body">
+                        <p>This is a web site for a class, CIT 336 at BYU-Idaho!</p>
+                    </div>
+                </div>
+                <div class="about-container">
+                    <div class="about-me">
+                        
+                    </div>
+                    <div class="about-me-image">
+                        
+                    </div>                  
+                </div>
+                <div>
                     
                 </div>
+                
             </main>
-            <aside role="complementary">
-                <div></div>
-            </aside>
+            
             <footer role="contentinfo">
-                <div>
+                <div class="contact">
                     <!--turn site plan into working link-->
                     <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/footer.php'; ?>
 
